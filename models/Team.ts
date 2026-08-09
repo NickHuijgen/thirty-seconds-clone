2fc3b5cf83020815f7b79a75b459a59ed595d942
NickRocketCode <nick@rocketcode.io>
2026-08-09 16:33:20 +0200

    [main] code style

--- models/Team.ts
+++ models/Team.ts
@@ -13,18 +13,19 @@ export default class Team {
         this.players = [];
     }
 
-    public addPlayer(player: Player) {
+    public addPlayer(player: Player): void {
         this.players.push(player);
     }
 
-    public removePlayer(player: Player) {
+    public removePlayer(player: Player): void {
         this.players = this.players.filter(p => p !== player);
     }
 
-    public generatePlayerIndexes() {
+    public generatePlayerIndexes(): void {
         // Shuffle the players array
         for (let i = this.players.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
+            // @ts-ignore
             [this.players[i], this.players[j]] = [this.players[j], this.players[i]];
         }
 
@@ -34,19 +35,19 @@ export default class Team {
         });
     }
 
-    public goNextTurn() {
+    public goNextTurn(): void {
         this.active_player_index = (this.active_player_index + 1) % this.players.length;
     }
 
-    public activePlayer() {
+    public activePlayer(): Player|undefined {
         return this.players.filter(t => t.index === this.active_player_index)[0];
     }
 
-    public inactivePlayers() {
+    public inactivePlayers(): Player[] {
         return this.players.filter(p => p !== this.activePlayer());
     }
 
-    public playerCount() {
+    public playerCount(): number {
         return this.players.length;
     }
 }
